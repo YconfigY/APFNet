f40baa407a2dfd66eec4418504d15857f9f89197
config <57167701+YconfigY@users.noreply.github.com>
2023-05-02 17:20:36 +0800

    Add some comments

--- modules/sample_generator.py
+++ modules/sample_generator.py
@@ -1,56 +1,60 @@
 import numpy as np
 from PIL import Image
 
-from utils import overlap_ratio
+from modules.utils.utils import overlap_ratio
 
 
 class SampleGenerator():
     def __init__(self, type_, img_size, trans=1, scale=1, aspect=None, valid=False):
         self.type = type_
-        self.img_size = np.array(img_size) # (w, h)
+        self.img_size = np.array(img_size)  # The size of original image, (w, h)
         self.trans = trans
         self.scale = scale
         self.aspect = aspect
         self.valid = valid
 
-    def _gen_samples(self, bb, n):
-        #
-        # bb: target bbox (min_x,min_y,w,h)
-        bb = np.array(bb, dtype='float32')
+    def _gen_samples(self, bbox, n):
+        
+        # bbox: target bbox (min_x,min_y,w,h)
+        bbox = np.array(bbox, dtype='float32')
 
-        # (center_x, center_y, w, h)
-        sample = np.array([bb[0] + bb[2] / 2, bb[1] + bb[3] / 2, bb[2], bb[3]], dtype='float32')
-        samples = np.tile(sample[None, :], (n ,1))
+        # (min_x,min_y,w,h) --> (center_x, center_y, w, h)
+        sample = np.array([bbox[0] + bbox[2] / 2, bbox[1] + bbox[3] / 2, bbox[2], bbox[3]], dtype='float32')
+        # sample.shape=(4,) ->sample[None, :].shape=(1,4) -> samples.shape=(n,4)
+        samples = np.tile(sample[None, :], (n, 1))  
 
-        # vary aspect ratio
+        # various aspect ratio
         if self.aspect is not None:
             ratio = np.random.rand(n, 2) * 2 - 1
             samples[:, 2:] *= self.aspect ** ratio
 
         # sample generation
         if self.type == 'gaussian':
-            samples[:, :2] += self.trans * np.mean(bb[2:]) * np.clip(0.5 * np.random.randn(n, 2), -1, 1)
+            samples[:, :2] += self.trans * np.mean(bbox[2:]) * np.clip(0.5 * np.random.randn(n, 2), -1, 1)
             samples[:, 2:] *= self.scale ** np.clip(0.5 * np.random.randn(n, 1), -1, 1)
 
         elif self.type == 'uniform':
-            samples[:, :2] += self.trans * np.mean(bb[2:]) * (np.random.rand(n, 2) * 2 - 1)
+            # samples[:, :2] are (center_x, center_y), sample location random transform or move
+            samples[:, :2] += self.trans * np.mean(bbox[2:]) * (np.random.rand(n, 2) * 2 - 1)
+            # samples[:, 2:] are (w, h), sample size random scale
             samples[:, 2:] *= self.scale ** (np.random.rand(n, 1) * 2 - 1)
 
         elif self.type == 'whole':
             m = int(2 * np.sqrt(n))
             xy = np.dstack(np.meshgrid(np.linspace(0, 1, m), np.linspace(0, 1, m))).reshape(-1, 2)
             xy = np.random.permutation(xy)[:n]
-            samples[:, :2] = bb[2:] / 2 + xy * (self.img_size - bb[2:] / 2 - 1)
+            samples[:, :2] = bbox[2:] / 2 + xy * (self.img_size - bbox[2:] / 2 - 1)
             samples[:, 2:] *= self.scale ** (np.random.rand(n, 1) * 2 - 1)
 
-        # adjust bbox range
+        # adjust bbox range, limiting the bbox size to between 10 and img_size-10
         samples[:, 2:] = np.clip(samples[:, 2:], 10, self.img_size - 10)
         if self.valid:
             samples[:, :2] = np.clip(samples[:, :2], samples[:, 2:] / 2, self.img_size - samples[:, 2:] / 2 - 1)
         else:
+            # limiting the bbox center position to between 0 and img_size
             samples[:, :2] = np.clip(samples[:, :2], 0, self.img_size)
 
-        # (min_x, min_y, w, h)
+        # (center_x, center_y, w, h) --> (min_x, min_y, w, h)
         samples[:, :2] -= samples[:, 2:] / 2
 
         return samples
@@ -62,25 +66,28 @@ class SampleGenerator():
 
         else:
             samples = None
-            remain = n
-            factor = 2
+            remain = n  # ?
+            factor = 2  # ?
             while remain > 0 and factor < 16:
-                samples_ = self._gen_samples(bbox, remain * factor)
+                _samples = self._gen_samples(bbox, remain * factor)
 
-                idx = np.ones(len(samples_), dtype=bool)
+                idx = np.ones(len(_samples), dtype=bool)  # generate full-one list
                 if overlap_range is not None:
-                    r = overlap_ratio(samples_, bbox)
-                    idx *= (r >= overlap_range[0]) * (r <= overlap_range[1])
+                    # Compute overlap ratio (IoU) between '_samples' and 'bbox'
+                    iou = overlap_ratio(_samples, bbox)
+                    # Filter 'index' of '_samples' whose IoU is between overlap thresholds
+                    idx *= (iou >= overlap_range[0]) * (iou <= overlap_range[1])
                 if scale_range is not None:
-                    s = np.prod(samples_[:, 2:], axis=1) / np.prod(bbox[2:])
+                    # s = the areas of '_samples' / the areas of 'bbox'
+                    s = np.prod(_samples[:, 2:], axis=1) / np.prod(bbox[2:])
                     idx *= (s >= scale_range[0]) * (s <= scale_range[1])
-
-                samples_ = samples_[idx, :]
-                samples_ = samples_[:min(remain, len(samples_))]
+                _samples = _samples[idx, :]  # Filter '_samples' by 'index
+                # Save the first 'min(remain, len(_samples))' '_samples'
+                _samples = _samples[:min(remain, len(_samples))] 
                 if samples is None:
-                    samples = samples_
+                    samples = _samples
                 else:
-                    samples = np.concatenate([samples, samples_])
+                    samples = np.concatenate([samples, _samples])
                 remain = n - len(samples)
                 factor = factor * 2
 
